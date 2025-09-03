55747e7afe0e779cb3e3f1607f716ec1d83e7a4d
savaariuttam <uttam@onlinesavaari.com>
2025-09-03 10:24:28 +0530

    test change in navbar

--- components/account/AccountPopover.tsx
+++ components/account/AccountPopover.tsx
@@ -14,7 +14,7 @@ import { cn } from "@/lib/utils";
 
 const AccountPopover = () => {
   const pathname = usePathname();
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
 
   const userLinks = [
     {
